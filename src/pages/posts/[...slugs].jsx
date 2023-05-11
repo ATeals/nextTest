78028564935e4833c4d0cd8136173b1d119a50fa
ateals <ateals@icloud.com>
2023-05-11 17:08:25 +0900

    콜랙션별로 마크다운 파일 분리

---
+++ src/pages/posts/[...slugs].jsx
@@ -0,0 +1,32 @@
+import { allPosts } from "contentlayer/gererated";
+import "github-markdown-css";
+import MarkdownPost from "@/components/blogPost/MarkdownPost";
+
+export default ({ post }) => {
+    return (
+        <>
+            <article className="post">
+                <h1>{post.title}</h1>
+                <div className="log">
+                    <MarkdownPost post={post.body.code} />
+                </div>
+            </article>
+        </>
+    );
+};
+
+export const getStaticPaths = async () => {
+    return {
+        paths: allPosts.filter((i) => !i._raw.sourceFilePath.includes("/index.mdx")).map((p) => ({ params: { slugs: p._raw.flattenedPath.split("/") } })),
+        fallback: "blocking",
+    };
+};
+
+export const getStaticProps = async ({ params }) => {
+    const post = allPosts.find((p) => p._raw.flattenedPath === params.slugs.join("/"));
+    return {
+        props: {
+            post,
+        },
+    };
+};
--- src/pages/posts/[slug].jsx
+++ src/pages/posts/[slug].jsx
@@ -1,71 +1,24 @@
 import { allPosts } from "contentlayer/gererated";
 import "github-markdown-css";
-import Toc from "@/components/blogPost/Toc";
-import MarkdownPost from "@/components/blogPost/MarkdownPost";
-import PreviewNav from "@/components/blogPost/PreviewNav";
 
-export default ({ post, preNavProp }) => {
-    return (
-        <>
-            <article className="post">
-                <h1>{post.title}</h1>
-                <Toc post={post} />
-                <div className="log">
-                    <MarkdownPost post={post.body.code} />
-                </div>
-
-                <PreviewNav preNavProp={preNavProp} />
-            </article>
-
-            <style jsx>{`
-                .post {
-                    display: flex;
-                    justify-content: center;
-                    font-size: 1rem;
-                    flex-direction: column;
-                    padding: 20px;
-                    margin: 20px;
-                    margib-bottom: 100px;
-                    border-radius: 50px;
-                }
-
-                h1 {
-                    font-size: 2em;
-                    font-weight: 700;
-                    display: flex;
-                    justify-content: center;
-                    margin: 50px;
-                }
-
-                .log {
-                    border-top: 1px solid #c6d6f5;
-                    padding-top: 10px;
-                }
-            `}</style>
-        </>
-    );
+export default ({ collection }) => {
+    console.log(collection);
+    return <div>hello</div>;
 };
 
 export const getStaticPaths = async () => {
     return {
-        paths: allPosts.map((p) => ({ params: { slug: p._raw.flattenedPath } })),
+        paths: allPosts.map((p) => ({ params: { slug: p._raw.flattenedPath.split("/")[0] } })),
         fallback: false,
     };
 };
 
 export const getStaticProps = async ({ params }) => {
-    const post = allPosts.find((p) => p._raw.flattenedPath === params.slug);
-    const index = allPosts.findIndex((p) => p._raw.flattenedPath === params.slug);
-    const prePost = allPosts[index + 1] || null;
-    const nextPost = allPosts[index - 1] || null;
-
+    // const collection = getCollactionItem(params.slug);
+    const collection = allPosts;
     return {
         props: {
-            post,
-            preNavProp: {
-                prePost,
-                nextPost,
-            },
+            collection,
         },
     };
 };

f4604b0a80a6be57c5b9056db9d75135a50dc163
ateals <ateals@icloud.com>
2023-07-07 18:02:48 +0900

    delete all

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./src/**/*.{js,ts,jsx,tsx,mdx,css}"],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-};
